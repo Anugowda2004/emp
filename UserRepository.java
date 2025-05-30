fd12eb9585f8be5eaf6ac65d2a97683edc021b6c
Anugowda2004 <151116804+Anugowda2004@users.noreply.github.com>
2025-05-30 11:23:36 +0530

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,7 +0,0 @@
-package userservice.repository;
-
-import userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
